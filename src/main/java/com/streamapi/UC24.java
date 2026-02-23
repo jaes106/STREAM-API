e80cd531d3c90c7237071e5cc38779cfd138bc72
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-02-23 15:34:19 +0530

    [Jason] Added: UC2.4: Filter Even Numbers and Store

--- src/main/java/com/streamapi/UC24.java
+++ src/main/java/com/streamapi/UC24.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class UC23 {
+public class UC24 {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
 
-        List<Integer> doubledList = numbers.stream()
-                .map(n -> n * 2)
+        List<Integer> evenList = numbers.stream()
+                .filter(n -> n % 2 == 0)
                 .collect(Collectors.toList());
 
-        System.out.println(doubledList);
+        System.out.println(evenList);
     }
 }
