5c4264eb941f83d9c09fdf9b0dee8976c5306da9
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-02-23 15:32:24 +0530

    [Jason] Added: UC2.2: Transform Each Element to Double (map)

--- src/main/java/com/streamapi/UC22.java
+++ src/main/java/com/streamapi/UC22.java
@@ -3,11 +3,12 @@ package main.java.com.streamapi;
 import java.util.Arrays;
 import java.util.List;
 
-public class UC21 {
+public class UC22 {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 
         numbers.stream()
-                .forEach(n -> System.out.println(n));
+                .map(n -> n * 2)
+                .forEach(System.out::println);
     }
 }
