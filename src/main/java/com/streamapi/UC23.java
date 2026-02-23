e68e41800c55f259f51925cec56f9dfbee031328
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-02-23 15:33:33 +0530

    [Jason] Added: UC2.3: Store Doubled Values into New List (collect)

--- src/main/java/com/streamapi/UC23.java
+++ src/main/java/com/streamapi/UC23.java
@@ -2,13 +2,16 @@ package main.java.com.streamapi;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class UC22 {
+public class UC23 {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 
-        numbers.stream()
+        List<Integer> doubledList = numbers.stream()
                 .map(n -> n * 2)
-                .forEach(System.out::println);
+                .collect(Collectors.toList());
+
+        System.out.println(doubledList);
     }
 }
