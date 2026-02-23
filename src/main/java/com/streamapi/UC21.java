025769db8593cc2c8704daf5e350bb5ceac80373
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-02-23 15:09:17 +0530

    [Jason] Added: UC2.1: Create Stream and Iterate (forEach)

---
+++ src/main/java/com/streamapi/UC21.java
@@ -0,0 +1,13 @@
+package main.java.com.streamapi;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UC21 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        numbers.stream()
+                .forEach(n -> System.out.println(n));
+    }
+}
